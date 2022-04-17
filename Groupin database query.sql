5b40e00557c05ff127391adb3e30151df264992c
Hajun Lee <wklee610@naver.com>
2022-04-17 16:28:51 +0900

    Add files via upload

---
+++ Groupin database query.sql
@@ -0,0 +1,52 @@
+USE Groupin;
+
+DROP TABLE if EXISTS user;
+
+CREATE TABLE `user` (
+	userID CHAR(9) NOT NULL PRIMARY KEY,
+	username VARCHAR(20) NOT NULL,
+	`PASSWORD` VARCHAR(20) NOT NULL,
+	`credit` INT NOT NULL,
+	wechat_num VARCHAR(30) NOT NULL,
+	phone_num INT NOT NULL,
+	school_email VARCHAR(26) NOT NULL,
+	second_email VARCHAR(30),
+	reg_dt DATETIME NOT NULL
+);
+
+
+
+DROP TABLE if EXISTS `group`;
+
+CREATE TABLE `group` (
+	groupID INT NOT NULL PRIMARY KEY,
+	userID CHAR(9) NOT NULL,
+	username VARCHAR(20) NOT NULL,
+	classID VARCHAR(10),
+	leaderID CHAR(9) NOT NULL,
+	reserved_quota INT NOT NULL,
+	school_email VARCHAR(26) NOT NULL,
+	description TEXT
+);                                                                          
+
+
+DROP TABLE if EXISTS `content`;
+
+CREATE TABLE `content` (
+	leaderID CHAR(9) NOT NULL PRIMARY KEY,
+	description TEXT,
+	groupID INT NOT NULL,
+	classID VARCHAR(10),
+	log_text TEXT
+);
+	
+	
+DROP TABLE if EXISTS `credit`;
+
+CREATE TABLE `credit` (
+	userID CHAR(9) NOT NULL PRIMARY KEY,
+	username VARCHAR(20) NOT NULL,
+	`credit` INT NOT NULL,
+	groupID INT NOT NULL
+);
+

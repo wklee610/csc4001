174600671c2d0ae01a4f323b71f663ae116c3c93
geedanegg <97676232+geedanegg@users.noreply.github.com>
2022-05-13 13:54:41 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (isset ( $_SESSION ["visitcode"] )){
-
+//having such session means the user is already logged in on this computer
     echo "Already login, redirecting to personal center...";
     header("Refresh:2;url=personal_center.php");
 
@@ -18,16 +18,16 @@ $password=($_POST['userpassword']);
 
 $sql="select * from user where username='$name'&&userpassword='$password' ";
 $result=mysqli_query($link,$sql);
-if($result==false){
+if($result==false){//no user with matched username and password
     echo "Login failed";
 }else{
     $row=mysqli_fetch_array($result);
     if($row!=null){
         $_SESSION["username"]=$name;
-	    $_SESSION["visitcode"]=$row["userID"];
+	    $_SESSION["visitcode"]=$row["userID"];//session of this logged in user starts, session name is username, session visitcode is user ID
         echo $name;
         echo " Login successfully";
-        header("Refresh:2;url=personal_center_page.html");
+        header("Refresh:2;url=personal_center_page.html");//redirecting to the personal center
     }else{
         echo "Login failed";
         header("Refresh:2;url=login_page.html");
